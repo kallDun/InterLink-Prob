a2940f775c72890cb2a9df2a8814542cc43548b6
kallDun <igorxlonline24@gmail.com>
2020-11-17 01:53:52 +0200

    Добавьте файлы проекта.

---
+++ InterLink Prob/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace InterLink_Prob
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(95, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(784, 561);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ InterLink Prob/Form1.cs
@@ -0,0 +1,142 @@
+﻿using Jitbit.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace InterLink_Prob
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string PATH = $"{Environment.CurrentDirectory}\\Final File.csv";
+        CsvExport csvExport = new CsvExport();
+        List<string> inputNames = new List<string> { };
+        List<string> inputDates = new List<string> { };
+        List<double> hours = new List<double> { };
+        List<string> names = new List<string> { };
+        List<string> dates = new List<string> { };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readDoc();
+            names = inputNames.Distinct().ToList();
+            dates = inputDates.Distinct().ToList();
+            writeDataInFile();
+            csvExport.ExportToFile(PATH);
+        }
+
+        private void setUpDataGrid()
+        {
+            DataGridViewTextBoxColumn dtColumn = new DataGridViewTextBoxColumn();
+            dtColumn.Name = "Name / Date";
+            dtColumn.HeaderText = "Name / Date";
+            dataGridView1.Columns.Add(dtColumn);
+
+            foreach (var date in dates)
+            {
+                DataGridViewTextBoxColumn dColumn = new DataGridViewTextBoxColumn();
+                dColumn.Name = $"Date {date}";
+                dColumn.HeaderText = date;
+                dataGridView1.Columns.Add(dColumn);
+            }
+        }
+
+        private void writeDataInFile()
+        {
+            setUpDataGrid();
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                dataGridView1.Rows.Add();
+                csvExport.AddRow();
+                csvExport["Name / Date"] = names[i];
+                dataGridView1.Rows[i].Cells[0].Value = names[i];
+
+                for (int j = 0; j < dates.Count; j++)
+                {
+                    var info = foundHours_byFile(names[i], dates[j]);
+                    csvExport[dates[j]] = info;
+                    dataGridView1.Rows[i].Cells[j + 1].Value = info;
+                }
+            }
+        }
+
+        private double foundHours_byFile(string name, string date)
+        {
+            for (int i = 0; i < inputNames.Count; i++)
+            {
+                if (inputNames[i] == name && inputDates[i] == date) return hours[i];
+            }
+
+            return 0;
+        }
+
+        private string remakeDate(string date)
+        {
+            var data = date.Split();
+            int month = 0;
+            switch (data[0])
+            {
+                case "Jan":
+                    month = 1;
+                    break;
+                case "Feb":
+                    month = 2;
+                    break;
+                case "Mar":
+                    month = 3;
+                    break;
+                case "Apr":
+                    month = 4;
+                    break;
+                case "May":
+                    month = 5;
+                    break;
+                case "Jun":
+                    month = 6;
+                    break;
+                case "Jul":
+                    month = 7;
+                    break;
+                case "Aug":
+                    month = 8;
+                    break;
+                case "Sep":
+                    month = 9;
+                    break;
+                case "Oct":
+                    month = 10;
+                    break;
+                case "Nov":
+                    month = 11;
+                    break;
+                case "Dec":
+                    month = 12;
+                    break;
+            }
+
+            return $"{data[2]}-{string.Format("{0:00}", month)}-{data[1]}";
+        }
+
+        private void readDoc()
+        {
+            string[] lines = File.ReadAllLines($"{Environment.CurrentDirectory}\\acme_worksheet.csv");
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var list = lines[i].Split(',').ToList<string>();
+
+                inputNames.Add(list[0]);
+                inputDates.Add(remakeDate(list[1]));
+                hours.Add(double.Parse(list[2]));
+            }
+        }
+    }
+}
